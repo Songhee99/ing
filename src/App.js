421d69943de90ea4cb05b77f800909a16a2fb466
하송희 <99heesong@naver.com>
2022-12-15 14:38:00 +0900

    기능추가: 게임 시작 멘트 출력

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+const OutputView = require("../../../precourse/javascript-bridge/src/OutputView");
+
 class App {
-  play() {}
+  play() {
+    this.runBaseballGame();
+  }
+
+  runBaseballGame() {
+    OutputView.printStartMent();
+  }
 }
+
+const app = new App();
+app.play();
 
 module.exports = App;
